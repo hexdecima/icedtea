dedd58d200bdb1027a2aeb3d749cbe1074c10694
Mai <mai@tilde.team>
2025-05-13 23:15:38 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "a tModloader 1.4 mod project.";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nil
+          nixfmt-classic
+          mono
+          dotnet-sdk
+        ];
+      };
+    };
+}
